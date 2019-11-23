1137d82dba5c6cf53689e97e2e2c394f72c08458
mamona-zahoor <uetian997@gmail.com>
2019-11-22 20:55:57 -0800

    Made changes to Academy script

--- Emotional AI/Assets/GameAcademy.cs
+++ Emotional AI/Assets/GameAcademy.cs
@@ -4,14 +4,47 @@ using UnityEngine;
 using MLAgents;
 public class GameAcademy : Academy {
 
-
-    public override void InitializeAcademy() { }
+    Animator AnimZombie;
+    public float Timepassed;
+    public Lara Lara;
+    public MarkoScript Marko;
+    public float MarkoFood;
+    public float MarkoHealth;
+    public AgentMove Hallo;
+    public float HalloFood;
+    public float HalloHealth;
+    public float LaraFood;
+    public float LaraHealth;
+    int seconds = 0;
+   // public PythonCommunicator py;
+    int i = 0;
+    int count = 0;
+    public GameObject Food1;
+    public GameObject Food2;
+    public GameObject Food3;
+   
+    public override void InitializeAcademy() {
+        MarkoFood = Marko.Food;
+        MarkoHealth = Marko.Health;
+        LaraFood = Lara.Food;
+        LaraHealth = Lara.Health;
+        HalloFood = Hallo.Food;
+        HalloHealth = Hallo.Health;
+    }
     public void SetEnvironment()
     { }
     public override void AcademyReset()
     { }
 
     public override void AcademyStep()
-    { }
+    {
+        Timepassed += Time.deltaTime;
+        seconds = (int)Timepassed;
+
+        if (seconds == 10)
+        {
+           Done();        
+        }
+    }
     }
 
